c79e22eeffdd9dadc5a3883c32c8cd7414833c12
BingHao <hohao129@gmail.com>
2023-09-14 05:14:26 +0800

    Finished feedback fields

--- lib/pages/feedback1.dart
+++ lib/pages/feedback1.dart
@@ -11,18 +11,26 @@ class Feedback1 extends StatefulWidget {
 class _FeedbackState extends State<Feedback1> {
   int _rating = 0;
   final List<String> categories = [
-  'Feedback',
-  'IT Department',
-  'Error',
-  'Customer Service',
-  'Design',
-  'Response Time'];
+    'Feedback',
+    'IT Department',
+    'Error',
+    'Customer Service',
+    'Design',
+    'Response Time'
+  ];
   late List<bool> selected;
+  final commentsController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    selected = List.generate(categories.length, (index) => false);
+  }
 
   @override
   Widget build(BuildContext context) {
     final max_width = MediaQuery.of(context).size.width;
-    final max_height = MediaQuery.of(context).size.height;
+    final max_height = MediaQuery.of(context).size.height - AppBar().preferredSize.height - MediaQuery.of(context).padding.top;
     return Scaffold(
       backgroundColor: Colors.white30,
       appBar: AppBar(
@@ -30,69 +38,94 @@ class _FeedbackState extends State<Feedback1> {
         backgroundColor: Colors.white54,
         foregroundColor: Colors.black,
       ),
-      body: Container(
-        width: max_width,
-        height: max_height,
-        color: Colors.grey,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            Padding(
-              padding: const EdgeInsets.only(top: 30),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: List.generate(5, (index) {
-                  return GestureDetector(
-                    onTap: () {
-                      setState(() {
-                        _rating = index + 1;
-                      });
-                    },
-                    child: Icon(
-                      index < _rating ? Icons.star : Icons.star_border,
-                      color: Colors.yellowAccent,
-                      size: max_width / 8,
-                    ),
-                  );
-                }),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(top: 30),
-              child: Wrap(
-                children: List.generate(categories.length, (index) {
-                  return GestureDetector(
-                    onTap: () {
-                      setState(() {
-                        selected = List.filled(categories.length, false);
-                        selected[index] = !selected[index];
-                      });
-                      debugPrint(categories[index]);
-                    },
-                    child: AnimatedContainer(
-                      duration: const Duration(milliseconds: 200),
-                      decoration: BoxDecoration(
-                        color: selected[index] ? Colors.cyanAccent : null,
-                        borderRadius: BorderRadius.circular(10),
-                        border: Border.all(
-                          color: Colors.black,
-                          width: 1.5,
-                        ),
+      body: SingleChildScrollView(
+        child: Container(
+          width: max_width,
+          height: max_height,
+          color: Colors.grey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(top: 30),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: List.generate(5, (index) {
+                    return GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          _rating = index + 1;
+                        });
+                      },
+                      child: Icon(
+                        index < _rating ? Icons.star : Icons.star_border,
+                        color: Colors.yellowAccent,
+                        size: max_width / 8,
                       ),
-                      margin: const EdgeInsets.all(5),
-                      padding: const EdgeInsets.all(5),
-                      child: Text(categories[index]),
-                    ),
-                  );
-                }),
+                    );
+                  }),
+                ),
               ),
-            ),
-            const Padding(
-              padding: EdgeInsets.only(top: 30),
-              child: Text("Please tell us what we should improve",
-                  textScaleFactor: 1.5),
-            ),
-          ],
+              Padding(
+                padding: const EdgeInsets.only(top: 30),
+                child: Wrap(
+                  children: List.generate(categories.length, (index) {
+                    return GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          selected = List.filled(categories.length, false);
+                          selected[index] = !selected[index];
+                        });
+                      },
+                      child: AnimatedContainer(
+                        duration: const Duration(milliseconds: 200),
+                        decoration: BoxDecoration(
+                          color: selected[index] ? Colors.cyanAccent : null,
+                          borderRadius: BorderRadius.circular(10),
+                          border: Border.all(
+                            color: Colors.black,
+                            width: 1.5,
+                          ),
+                        ),
+                        margin: const EdgeInsets.all(5),
+                        padding: const EdgeInsets.all(5),
+                        child: Text(categories[index]),
+                      ),
+                    );
+                  }),
+                ),
+              ),
+              const Padding(
+                padding: EdgeInsets.only(top: 30),
+                child: Text("Please tell us what we should improve",
+                    textScaleFactor: 1.5),
+              ),
+              Container(
+                width: max_width - 50,
+                height: max_height / 2.5,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  border: Border.all(
+                    color: Colors.black,
+                    width: 1.5,
+                  ),
+                ),
+                margin: const EdgeInsets.all(5),
+                padding: const EdgeInsets.all(5),
+                child: TextField(
+                  decoration: const InputDecoration(
+                    hintText: 'Write comment here...',
+                    border: InputBorder.none,
+                  ),
+                  controller: commentsController,
+                ),
+              ),
+              ElevatedButton(
+                onPressed: () {},
+                child: const Text('Submit'),
+              ),
+            ],
+          ),
         ),
       ),
     );
